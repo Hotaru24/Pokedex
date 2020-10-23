93305a5d12930fac8e2ae42abf3a435b7b83b1cb
Hotaru24 <florent-pasquet@protonmail.com>
2020-10-23 12:23:32 +0200

    project start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/pokemon`)
+    .then((result) => {
+      console.log(result.data)
+    })
+  }) 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>The Great Pokedex</h1>
       </header>
     </div>
   );
