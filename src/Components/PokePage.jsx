ba4efd5170fe7ead702479169b407a625c7f2979
Hotaru24 <florent-pasquet@protonmail.com>
2020-12-21 17:26:03 +0100

    PokePage API call is ok

--- src/Components/PokePage.jsx
+++ src/Components/PokePage.jsx
@@ -1,42 +1,49 @@
-import React, {useContext, useState, useEffect} from 'react';
-import pokeCtx from "./pokeCtx";
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
-const PokePage = (props) => {
+const PokePage = () => {
     
-  const [pokeList] = useContext(pokeCtx);
-  const [pokeId, setPokeId] = useState(0);
+  const [pokeId, setPokeId] = useState(1);
+  const [pokeData, setPokeData] = useState([])
   const reverseUrl = window.location.href.split("").reverse().join("");
+  const pokeImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`
 
   
   useEffect(() => {
-    if (reverseUrl.substr(1, 2)  == "/"){
+    if (reverseUrl.substr(1, 2)  === "/"){
       setPokeId(reverseUrl.substr(0, 1).split("").reverse().join(""));
     } else {
       setPokeId(reverseUrl.substr(0, 2).split("").reverse().join(""));
     }        
   },[]);
 
-  return (
-    <>
-      <div id="pageBody">
-        {/* rajouter un filter pour ne récupérer 
-        les données du pokemon dont l'id est = à 
-        pokeId  */}
-        {pokeList
-        
-        .map(
-          (poke, index) => {
-            return ( 
-              <div key={index}>
-                <p>{`totopage ${pokeId}`}</p>
-              </div> 
-            )
-          }
-        )}
-      </div>
-    </>
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`)
+    .then((result, err) => {
+        if (err) {
+          throw err;
+        } else {
+          setPokeData(result.data)      
+        }
+    })    
+  },[pokeId]);
 
-)
+  console.log(pokeData.types)
+
+  return (
+    <>        
+      <h1>{pokeData.name}</h1>
+        <div id="pageBody">
+          <img  src={pokeImg}  alt={pokeData.name}/>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+    </>
+  )
 };
 
 export default PokePage;
